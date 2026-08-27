e5339df342a31e6f11f511d6e204f6d634059bed
Virgile AE <ae.virgile@gmail.com>
2026-08-27 15:07:36 +0100

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.py
@@ -0,0 +1,21 @@
+def area(index1: int, index2: int, heights: list[int]) -> int:
+    return abs(index1 - index2) * min(heights[index1], heights[index2])
+
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
+        l, r = 0, len(heights) - 1
+
+        while l < r:
+            a = area(l, r, heights)
+
+            if a > max_area:
+                max_area = a
+
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return max_area
