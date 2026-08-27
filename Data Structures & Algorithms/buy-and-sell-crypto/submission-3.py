be8ae97efea889592b1102bdf89f58d33bce5863
Virgile AE <ae.virgile@gmail.com>
2026-08-27 17:09:19 +0100

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+
+        l, r = 0, 1
+
+        while l < len(prices) and r < len(prices):
+            if prices[r] < prices[l]:
+                l = r
+                r += 1
+            else:
+                max_profit = max(max_profit, prices[r] - prices[l])
+                r += 1
+
+        return max_profit
