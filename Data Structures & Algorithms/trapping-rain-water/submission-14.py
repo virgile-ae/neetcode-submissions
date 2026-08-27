e39fbdbfa8a80c6b1eb18268c7e447b5461ba9a3
Virgile AE <ae.virgile@gmail.com>
2026-08-27 16:21:52 +0100

    Add: trapping-rain-water - submission-14

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-14.py
@@ -0,0 +1,28 @@
+from itertools import pairwise
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        filtered = []
+        stack = []
+
+        for i in range(len(height)):
+            if not stack:
+                stack.append(i)
+                continue
+
+            while len(stack) > 1 and height[stack[-1]] < height[i]:
+                stack.pop()
+
+            if height[stack[-1]] < height[i]:
+                filtered.append(stack.pop())
+
+            stack.append(i)
+            
+        total_area = 0
+        for l, r in pairwise(sorted(filtered + stack)):
+            total_area += min(height[l], height[r]) * (r - l - 1)
+            for h in height[l + 1:r]:
+                total_area -= h
+    
+        return total_area
+
